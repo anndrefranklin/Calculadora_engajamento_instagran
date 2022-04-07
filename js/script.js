a932cc937c32938812bb2424d523ec18201e9af7
RaioDigital <raiodigitalweb@gmail.com>
2022-04-06 22:45:55 -0300

    Commit inicial

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Calcular a formula - ((curtidas + comentarios) / seguidores) * 100
+//console.log('Flavio Teste')
+const curtidas = document.querySelector("#curtidas")
+const comentarios = document.querySelector("#comentarios")
+const seguidores = document.querySelector("#seguidores")
+const resultado = document.querySelector("#resultado")
+
+function calcular(){
+  //console.log(curtidas.value)
+  resultado.value = ((parseInt(curtidas.value) + parseInt(comentarios.value)) / parseInt(seguidores.value)) * 100
+  //console.log(seguidores.value)
+  document.getElementById("resultado").innerHTML = resultado.value
+}
+
+function limparCampos(){
+    with (document) {
+        getElementById("curtidas").value = "";
+        getElementById("comentarios").value = "";
+        getElementById("seguidores").value = "";        
+        getElementById("resultado").innerHTML = "???";
+        curtidas.focus();
+    }
+}
